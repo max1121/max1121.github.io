e50b1f807ef8f239252721ea51cbfcc18dab45f1
umesh <Umesh@192.168.1.100>
2017-07-24 19:38:09 +0100

    Error

---
+++ SmartThoughts/js/fadeslideshow.js
@@ -0,0 +1,15 @@
+/* Ultimate Fade-in slideshow (v2.4)
+* Last updated: May 24th, 2010. This notice must stay intact for usage 
+* Author: Dynamic Drive at http://www.dynamicdrive.com/
+* Visit http://www.dynamicdrive.com/ for full source code
+*/
+
+//Oct 6th, 09' (v2.1): Adds option to randomize display order of images, via new option displaymode.randomize
+//May 24th, 10' (v2.4): Adds new "peakaboo" option to "descreveal" setting. oninit and onslide event handlers added.
+
+
+//No need to edit beyond here...
+
+jQuery.noConflict()
+
+
